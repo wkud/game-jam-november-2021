d7d921e7cf009d08e7542c6b028b7b78671659ad
filip-wan <51719931+filip-wan@users.noreply.github.com>
2021-11-13 02:50:42 +0100

    Add Enum SkillTargetCountType

--- Gamejam/Assets/Scripts/Logic/Skills/SkillData.cs
+++ Gamejam/Assets/Scripts/Logic/Skills/SkillData.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SkillData", menuName = "ScriptableObjects/SkillData", order = 1)]
 public class SkillData : ScriptableObject
 {
+
+
   [SerializeField] private string _name;
   [SerializeField] private string _description;
   [SerializeField] private int _power = 9001;
   [SerializeField] private int _cooldown = 5;
+  [SerializeField] private SkillTargetCountType _targetCountType;
 
   public string Name { get => _name; set => _name = value; }
   public int Power { get => _power; set => _power = value; }
   public int Cooldown { get => _cooldown; set => _cooldown = value; }
   public string Description { get => _description; set => _description = value; }
+  public SkillTargetCountType TargetCountType { get => _targetCountType; set => _targetCountType = value; }
 }
---
+++ Gamejam/Assets/Scripts/Logic/Skills/SkillTargetCountType.cs
@@ -0,0 +1,5 @@
+public enum SkillTargetCountType
+{
+  one,
+  all
+}
