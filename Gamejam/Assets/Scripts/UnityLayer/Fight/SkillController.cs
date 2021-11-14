ab9b13ba140c4bf6f8d4c81e8b2e9722fdda90fe
Wojciech <vopox07@gmail.com>
2021-11-14 19:18:40 +0100

    Fix Tooltip

--- Gamejam/Assets/Scripts/UnityLayer/Fight/SkillController.cs
+++ Gamejam/Assets/Scripts/UnityLayer/Fight/SkillController.cs
@@ -7,34 +7,52 @@ using UnityEngine.UI;
 public class SkillController : MonoBehaviour, IDescriptable
 {
     private Unit _unit;
-
+    private Image _image;
+    private Button _button;
 
     [SerializeField] private int _skillSlotNumber; // values from 0 to 2 (for 3 slots)
     public int SkillSlotNumber => _skillSlotNumber;
 
-    private bool _isActive = true;
-    public bool IsActive
+    private Player _player => (_unit?.Entity as Player);
+
+
+    private bool _isInteractable = true;
+    public bool IsInteractable
     {
-        get => _isActive;
+        get => _isInteractable;
         private set
         {
-            _isActive = value;
-            gameObject.SetActive(_isActive);
+            _isInteractable = value;
+            _button.interactable = _isInteractable;
         }
     }
 
-    public string Description => (_unit?.Entity as Player)?.GetSkillDescription(_skillSlotNumber);
+    public string Description => _player?.GetSkill(_skillSlotNumber)?.GetTooltipDescription();
+
+    public string Title => _player?.GetSkill(_skillSlotNumber)?.GetTooltipTitle();
+
+    public bool IsShowable => _isInteractable;
+
 
     public void Initialize(Unit unit)
     {
         _unit = unit;
+        _image = GetComponent<Image>();
+        _button = GetComponent<Button>();
+
         var button = GetComponent<Button>();
         button.onClick.AddListener(OnClick);
     }
 
     public void OnClick() => _unit.OnSkillClick(_skillSlotNumber);
 
-    public void Hide() => IsActive = false;
-    public void Show() => IsActive = true;
+    public void SetInteractable(bool interactable)
+    {
+        IsInteractable = interactable;
+    }
 
+    public void UpdateSprite()
+    {
+        _image.sprite = _player.GetSkill(_skillSlotNumber).Data.Sprite;
+    }
 }
