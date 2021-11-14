e97025f2fcd0982d5a515db1a3da9ab9e0c8aa2f
filip-wan <51719931+filip-wan@users.noreply.github.com>
2021-11-14 13:24:11 +0100

    Add constructor to Entity

--- Gamejam/Assets/Scripts/Logic/Entities/Entity.cs
+++ Gamejam/Assets/Scripts/Logic/Entities/Entity.cs
@@ -1,6 +1,12 @@
 public abstract class Entity
 {
     protected EntityStats _stats;
+
+    protected Entity(EntityStats stats)
+    {
+        _stats = stats;
+    }
+
     public EntityStats Stats => _stats;
 
     public void TakeDamage(int damage)
